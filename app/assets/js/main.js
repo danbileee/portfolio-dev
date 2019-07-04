099712b0223864266c93ed8049d9c4755ff75c01
Danbi Lee <blocktobody@gmail.com>
2019-07-05 04:37:01 +0900

    add basic setting & create static pages

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -14,6 +14,48 @@ $(document).ready(function() {
 
     return false;
   });
-  
+
+  //---------------------------------
+  // Header
+  //---------------------------------
+
+  $('.js-menu').on('click', function () {
+    $('html').toggleClass('menu-is-open');
+  });
+
+  //---------------------------------
+  // Main Swiper
+  //---------------------------------
+
+  var slideContainer = new Swiper('.js-mainslider', {
+    slidesPerView: 6.5,
+    initialSlide: 0,
+    spaceBetween: 60,
+    centeredSlides: true,
+    loop: true,
+    allowTouchMove: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      1201: {
+        slidesPerView: 5.3,
+        spaceBetween: 55
+      },
+      981: {
+        slidesPerView: 4.3,
+        spaceBetween: 50
+      },
+      767: {
+        slidesPerView: 3.8,
+        spaceBetween: 40
+      },
+      521: {
+        slidesPerView: 2.3,
+        spaceBetween: 40
+      }
+    }
+  });
 
 });
